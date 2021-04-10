4f09495b824ff6e506a6212f1bc070a692ea5234
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-04-10 04:38:38 +0300

    App is ready

---
+++ TaskulamppuXamarin.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TaskulamppuXamarin.Android")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskulamppuXamarin.Android")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Add some common permissions, these can be removed if not needed
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+// Taskulamppua varten laitettuja lupia
+[assembly: UsesPermission(Android.Manifest.Permission.Flashlight)]
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+// Vibraa varten ...
+[assembly: UsesPermission(Android.Manifest.Permission.Vibrate)]
---
+++ TaskulamppuXamarin/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Essentials; // Essentialsin kautta on pääsy laitteen toimintoihin
+
+/* https://docs.microsoft.com/en-us/xamarin/essentials/flashlight?tabs=android */
+
+namespace TaskulamppuXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void On_Button_Clicked(object sender, EventArgs e)
+        {
+            await Flashlight.TurnOnAsync();
+        }
+
+        private async void Off_Button_Clicked(object sender, EventArgs e)
+        {
+            await Flashlight.TurnOffAsync();
+        }
+
+        private void VibraOn_Button_Clicked(object sender, EventArgs e)
+        {
+            Vibration.Vibrate();
+        }
+
+        private void VibraOff_Button_Clicked(object sender, EventArgs e)
+        {
+            Vibration.Cancel();
+        }
+    }
+}
